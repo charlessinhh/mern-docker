5f47a4980c2760045fc571b1344a250feb2e4501
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-05-10 21:58:24 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   }, []);
 
   const handleLogin = async () => {
-    let result = await fetch("/api/login", {
+    let result = await fetch("http://backend:5000/api/login", {
       method: "post",
       body: JSON.stringify({ email, password }),
       headers: {
